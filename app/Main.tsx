d2f2ef1f91c7a654632b93a6e5478e229959508e
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-09-23 22:34:34 +0200

    Add example posts

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,12 +12,12 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 pt-16 dark:divide-gray-700">
         <div className="mx-8 bg-gray-400 bg-opacity-20 p-8 dark:bg-gray-800 dark:bg-opacity-50">
           <div className="space-y-2 pb-8 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            {/* <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
               Latest
             </h1>
             <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
               {siteMetadata.description}
-            </p>
+            </p> */}
           </div>
           <ul className="divide-y divide-gray-200 dark:divide-gray-700">
             {!posts.length && 'No posts found.'}
