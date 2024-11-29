ec3050cf578378c986654baa89aee1de7b77e501
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-11-29 16:22:56 +0300

    Edit 2024 posts

--- layouts/PostBanner.tsx
+++ layouts/PostBanner.tsx
@@ -59,7 +59,7 @@ export default function PostMinimal({ content, authorDetails, next, prev, childr
               <dl className="py-4">
                 <dt className="sr-only">Authors</dt>
                 <dd>
-                  <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+                  <ul className="flex flex-row flex-wrap justify-center gap-4 sm:space-x-8">
                     {authorDetails.map((author) => (
                       <li className="flex items-center space-x-2" key={author.name}>
                         {author.avatar && (
