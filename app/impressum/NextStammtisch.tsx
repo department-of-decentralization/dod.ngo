afd0d6acf7373c9f12b5dd169346ed4cb56d1467
Afri Schoedon <58883403+q9f@users.noreply.github.com>
2025-04-17 11:24:02 +0200

    stammtisch: add hour to the calculation in utc

--- app/impressum/NextStammtisch.tsx
+++ app/impressum/NextStammtisch.tsx
@@ -1,31 +1,30 @@
 'use client'
 
 export default function NextStammtisch() {
-  const currentDate = new Date();
-  const currentMonth = currentDate.getUTCMonth();
-  const currentYear = currentDate.getUTCFullYear();
-  let nextStammtisch = new Date();
-  let nextMonth = 0;
+  const currentDate = new Date()
+  const currentMonth = currentDate.getUTCMonth()
+  const currentYear = currentDate.getUTCFullYear()
+  let nextStammtisch = new Date()
+  let nextMonth = 0
 
   do {
-
     // Create date object for 1st of current month
-    nextStammtisch = new Date(Date.UTC(currentYear, currentMonth + nextMonth, 1));
+    nextStammtisch = new Date(Date.UTC(currentYear, currentMonth + nextMonth, 1))
 
     // Find first Wednesday
     while (nextStammtisch.getUTCDay() !== 3) {
-      nextStammtisch.setUTCDate(nextStammtisch.getUTCDate() + 1);
+      nextStammtisch.setUTCDate(nextStammtisch.getUTCDate() + 1)
     }
 
     // Add 2 weeks to get to 3rd Wednesday
-    nextStammtisch.setUTCDate(nextStammtisch.getUTCDate() + 14);
+    nextStammtisch.setUTCDate(nextStammtisch.getUTCDate() + 14)
 
     // Set start time to 21:00 UTC which is intentially a bit into the event
-    nextStammtisch.setUTCHours(21);
+    nextStammtisch.setUTCHours(21)
 
     // If Stammtisch already started/passed this month, move to next month
-    nextMonth++;
-  } while (nextStammtisch < currentDate);
+    nextMonth++
+  } while (nextStammtisch < currentDate)
 
   return (
     <span className="font-medium">
