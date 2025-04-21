b2232f0cfc5850bdf979685e37fd1d7fd4e420b7
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-04-21 13:07:20 +0200

    Reduce max-width

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,7 +102,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="mx-auto max-w-[100rem] bg-butter-400 text-black antialiased dark:bg-gray-950 dark:text-white">
+      <body className="mx-auto max-w-7xl bg-butter-400 text-black antialiased dark:bg-gray-950 dark:text-white">
         <ButtonScript />
         <ThemeProviders>
           <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
